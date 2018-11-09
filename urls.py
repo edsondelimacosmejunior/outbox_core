17ab234d80a1bdad1e693b54f6def9c85f694a91
edsonjunior <edsondelimajunior@gmail.com>
2018-11-09 16:46:23 -0300

    Ajustando cadastro de paises

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PaisList
+from .views import PaisList, PaisCreate
 
 urlpatterns = [
-    path('pais/', PaisList.as_view(), name='pais_list')
+    path('pais/', PaisList.as_view(), name='pais_list'),
+    path('pais/create/', PaisCreate.as_view(), name='pais_create')
 ]
--- views.py
+++ views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 from .models import Pais
 
 
 class PaisList(ListView):
     model = Pais
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super(PaisList, self).get_context_data(**kwargs)
+        context.update({
+            'title': "Países",
+            'subtitle': "Listagem dos Paises"})
+        return context
+
+
+class PaisCreate(CreateView):
+    model = Pais
+    fields = ['nome', 'codigo', 'codigo_bc']
+    success_url = reverse_lazy('pais_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(PaisCreate, self).get_context_data(**kwargs)
+        context.update({
+            'title': "Países",
+            'subtitle': "Listagem dos Paises"})
+        return context
